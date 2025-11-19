14daf4915a7cd8af28c62ff42581f8090108addd
MarkoG111 <markogacanovic111@gmail.com>
2025-11-19 23:46:59 +0100

    fix h!

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -10,12 +10,12 @@ import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "blog-a6b98.firebaseapp.com",
-  projectId: "blog-a6b98",
-  storageBucket: "blog-a6b98.appspot.com",
-  messagingSenderId: "311198757906",
-  appId: "1:311198757906:web:3023a83a49eeb68fa494cb",
-  measurementId: "G-TJWVQ5W4KH"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
