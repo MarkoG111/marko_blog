c4e56fdaaa1c86754f517867369f8010ce8dd0d5
MarkoG111 <markogacanovic111@gmail.com>
2025-11-22 20:50:47 +0100

    vite.config refactor

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,32 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const proxyPaths = [
-  '/api/posts',
-  '/api/categories',
-  '/api/notifications',
-  '/api/auth',
-  '/api/users',
-  '/api/comments',
-  '/api/likes',
-  '/api/images',
-  '/api/followers',
-  '/api/authorrequests',
-  '/api/usecaselogs',
-  '/api/register',
-  '/api/login'
-]
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
 
-const createProxyConfig = (path) => ({
-  target: 'http://localhost:5000',
-  secure: false,
-  changeOrigin: true,
-  ws: path === '/api/notificationsHub',
-})
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: Object.fromEntries(proxyPaths.map((path) => [path, createProxyConfig(path)])),
-  },
-})
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+            secure: false,
+          },
+          '/api/notificationsHub': {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+            secure: false,
+            ws: true
+          }
+        },
+      }
+      : undefined
+  };
+});
